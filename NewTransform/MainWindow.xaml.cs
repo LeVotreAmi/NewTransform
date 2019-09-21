5073a69899db171e7bf287b5735672422e0f88a2
LeVotreAmi <levotreami@gmail.com>
2019-09-21 10:36:09 +0300

    Initial commit

---
+++ NewTransform/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewTransform
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void XlsxDropPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+        }
+
+        void XlsxDropPanel_DragLeave(object sender, DragEventArgs e)
+        {
+
+        }
+
+        void XlsxDropPanel_DragOver(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void XlsxDropPanel_Drop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            xlsxLabel.Content = files[0].ToString();
+        }
+    }
+}
