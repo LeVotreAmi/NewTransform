59a597a8765faa2bc12ed623cebf0ecc6522632e
LeVotreAmi <levotreami@gmail.com>
2019-09-23 07:17:58 +0300

    Add convert doc to pdf and fix bugs

--- NewTransform/MainWindow.xaml.cs
+++ NewTransform/MainWindow.xaml.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using WinForms = System.Windows.Forms;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Win32;
 using Excel = Microsoft.Office.Interop.Excel;
 using Word = Microsoft.Office.Interop.Word;
+using OfficeConvert;
 
 namespace NewTransform
 {
@@ -23,12 +18,10 @@ namespace NewTransform
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Пути к файлам
         static string pathDoc = "";
         static string pathXls = "";
         static string pathSave = "";
 
-        //Свойства кнопки "Сгенерировать"
         private void Properties(Button generatedButton)
         {
             generatedButton.IsEnabled = true;
@@ -41,126 +34,138 @@ namespace NewTransform
 
         private void SaveFolderPanel_Drop(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            try
             {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                string file = (string)files[0];
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    string file = (string)files[0];
 
-                SaveFolderLabel.Content = file;
-                pathSave = file;
-            }
+                    SaveFolderLabel.Content = file;
+                    pathSave = file;
+                }
 
-            if (pathSave != "" && pathDoc != "" && pathXls != "")
+                if (pathSave != "" && pathDoc != "" && pathXls != "")
+                {
+                    Properties(generatedButton);
+                }
+            } catch
             {
-                Properties(generatedButton);
+                MessageBox.Show("Что-то пошло не так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            
         }
 
         private void XlsxDropPanel_Drop(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            try
             {
-                xlsxLabel.Visibility = Visibility.Hidden;
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                string file = (string)files[0];
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    xlsxLabel.Visibility = Visibility.Hidden;
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    string file = (string)files[0];
 
-                xlsxLabelTitle.IsEnabled = true;
-                xlsxLabelTitle.Content = file;
-                pathXls = file;
-            }
+                    xlsxLabelTitle.IsEnabled = true;
+                    xlsxLabelTitle.Content = file;
+                    pathXls = file;
+                }
 
-            if (pathSave != "" && pathDoc != "" && pathXls != "")
+                if (pathSave != "" && pathDoc != "" && pathXls != "")
+                {
+                    Properties(generatedButton);
+                }
+            } catch
             {
-                Properties(generatedButton);
+                MessageBox.Show("Что-то пошло не так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            
         }
 
         private void DocDropPanel_Drop(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            try
             {
-                docLabel.Visibility = Visibility.Hidden;
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                string file = (string)files[0];
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    docLabel.Visibility = Visibility.Hidden;
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    string file = (string)files[0];
 
-                docLabelTitle.IsEnabled = true;
-                docLabelTitle.Content = file;
-                pathDoc = file;
-            }
+                    docLabelTitle.IsEnabled = true;
+                    docLabelTitle.Content = file;
+                    pathDoc = file;
+                }
 
-            if (pathSave != "" && pathDoc != "" && pathXls != "")
+                if (pathSave != "" && pathDoc != "" && pathXls != "")
+                {
+                    Properties(generatedButton);
+                }
+            } catch
             {
-                Properties(generatedButton);
+                MessageBox.Show("Что-то пошло не так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            
         }
 
         private void Generate()
         {
-            var WordApp = new Word.Application();
-            WordApp.Visible = false;
-
-            //Открыть ворд && открыть эксель, получить 1 лист
-            var WordDocument = WordApp.Documents.Open(@"" + pathDoc);
-            Excel.Application ObjWorkExcel = new Excel.Application();
-            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@"" + pathXls);
-            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
-
-            //Получить кол-во заполненных ячеек эксель
-            int nInLastRow = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
-                System.Reflection.Missing.Value, System.Reflection.Missing.Value,
-                Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious,
-                false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
-            int nInLastCol = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
-                System.Reflection.Missing.Value, System.Reflection.Missing.Value,
-                Excel.XlSearchOrder.xlByColumns, Excel.XlSearchDirection.xlPrevious,
-                false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Column;
-
-            string[,] list = new string[nInLastRow, nInLastCol]; //Равен по размеру листу
-
-            //Данные с листа в массив
-            for (int i = 0; i < nInLastRow; i++)
-            {
-                for (int j = 0; j < nInLastCol; j++)
-                {
-                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString();
-
-                }
-            }
-
-            //Данные из массива в ворд
             try
             {
+                var WordApp = new Word.Application();
+                WordApp.Visible = false;
+
+                var WordDocument = WordApp.Documents.Open(@"" + pathDoc);
+                Excel.Application ObjWorkExcel = new Excel.Application();
+                Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@"" + pathXls);
+                Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+
+                int nInLastRow = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
+                    System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                    Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious,
+                    false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
+                int nInLastCol = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
+                    System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                    Excel.XlSearchOrder.xlByColumns, Excel.XlSearchDirection.xlPrevious,
+                    false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Column;
+
+                string[,] list = new string[nInLastRow, nInLastCol];
+
+                for (int i = 0; i < nInLastRow; i++)
+                {
+                    for (int j = 0; j < nInLastCol; j++)
+                    {
+                        list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString();
+
+                    }
+                }
+
                 for (int i = 0; i < nInLastRow; i++)
                 {
                     for (int j = 0; j < nInLastCol; j++)
                     {
 
-                        string tmp = list[i, j]; //Значение текущей ячейки в переменную
-                        ReplaceWordStub("{" + j + "}", tmp, WordDocument); //Меняем метку в шаблоне ворд на значение
+                        string tmp = list[i, j];
+                        ReplaceWordStub("{" + j + "}", tmp, WordDocument);
 
-                        //Сохранить, когда будет обрабатываться последняя ячейка в строке
                         if (j == nInLastCol - 1)
                         {
                             WordDocument.SaveAs(@"" + pathSave + "\\" + list[i, j] + ".docx");
-                            //Закрыть, иначе будет множество экземпляров ворд в фоне, открыть шаблон снова для следующей строки
+
                             WordDocument.Close(false, Type.Missing, Type.Missing);
                             WordDocument = WordApp.Documents.Open(@"" + pathDoc);
                         }
 
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Что-то пошло не так", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            finally
-            {
-                //Закрыть после завершения работы
                 WordDocument.Close(false, Type.Missing, Type.Missing);
                 WordApp.Quit();
                 ObjWorkBook.Close(false, Type.Missing, Type.Missing);
                 ObjWorkExcel.Quit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Возможно какой-то из выбранных файлов открыт. Закройте файл и повторите попытку.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
@@ -175,11 +180,6 @@ namespace NewTransform
             Panel.SetZIndex(imgGenerate, -1);
         }
 
-        private void XlsxPanelDropPanel_Click(object sender, RoutedEventArgs e)
-        {
-            xlsxLabelTitle.Content = "Test";
-        }
-
         static void ReplaceWordStub(string StubToReplace, string Text, Word.Document WordDocument)
         {
 
@@ -187,32 +187,112 @@ namespace NewTransform
             Range.Find.Execute(FindText: StubToReplace, ReplaceWith: Text);
 
         }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            GC.Collect();
-        }
         
         private void XlsxDropPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            /*
             OpenFileDialog ofd = new OpenFileDialog();
             
             ofd.Filter = "Файлы xlsx|*.xlsx";
             ofd.ShowDialog();
 
             pathXls = ofd.FileName;
-            //Если все пути получены: включаем кнопку "Сгенерировать"
             if (pathXls != "" && pathDoc != "" && pathSave != "")
             {
                 Properties(generatedButton);
             }
-            */
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void DocDropPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Файлы docx|*.docx|Файлы doc|*.doc";
+            ofd.ShowDialog();
+
+            pathDoc = ofd.FileName;
+            if (pathXls != "" && pathDoc != "" && pathSave != "")
+            {
+                Properties(generatedButton);
+            }
+        }
+
+        private void SaveFolderPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            WinForms.FolderBrowserDialog fbd = new WinForms.FolderBrowserDialog();
+            fbd.ShowDialog();
+
+            pathSave = fbd.SelectedPath;
+            if (pathXls != "" && pathDoc != "" && pathSave != "")
+            {
+                Properties(generatedButton);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            GC.Collect();
+        }
+
+        private void tryConvert(Converter converter, String inputFile, String outputFile)
+        {
+            try
+            {
+                converter.Convert(inputFile, outputFile);
+            }
+            catch (ConvertException err)
+            {
+                MessageBox.Show(err.Message + "\n\n" + err.StackTrace);
+            }
+        }
+
+        private void ConvertPanel_Drop(object sender, DragEventArgs e)
+        {
+            ConvertPanel.IsEnabled = false;
+
+            List<string> paths = new List<string>();
+            foreach (string obj in (string[])e.Data.GetData(DataFormats.FileDrop))
+                if (Directory.Exists(obj))
+                    paths.AddRange(Directory.GetFiles(obj, "*.*", SearchOption.AllDirectories));
+                else
+                    paths.Add(obj);
+
+            int count = paths.Count;
+            for (int i = 0; i < count; i++)
+            {
+                String inputFile = paths[i];
+                int lngth = inputFile.Length;
+                if (inputFile[lngth - 1] == 'c' || inputFile[lngth - 2] == 'c')
+                {
+                    String outputFile = String.Concat(inputFile, ".pdf");
+                    Converter converter = new WordConverter();
+                    tryConvert(converter, inputFile, outputFile);
+                }
+            }
+            if(count > 0) MessageBox.Show("Конвертация завершена", "Готово");
+            ConvertPanel.IsEnabled = true;
+        }
+
+        private void ConvertPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ConvertPanel.IsEnabled = false;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Файлы docx|*.docx|Файлы doc|*.doc";
+            ofd.ShowDialog();
+
+            int count = ofd.FileNames.Length;
+            for (int i = 0; i < count; i++)
+            {
+                String inputFile = ofd.FileNames[i];
+                int lngth = inputFile.Length;
+                if (inputFile[lngth - 1] == 'c' || inputFile[lngth - 2] == 'c')
+                {
+                    String outputFile = String.Concat(inputFile, ".pdf");
+                    Converter converter = new WordConverter();
+                    tryConvert(converter, inputFile, outputFile);
+                }
+            }
+            if (count > 0) MessageBox.Show("Конвертация завершена", "Готово");
+            ConvertPanel.IsEnabled = true;
         }
     }
 }
